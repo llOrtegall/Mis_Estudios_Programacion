e142be1d615a8500a113c89ceb5798ba9160c622
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-02-08 21:14:49 -0500

    proyect ok

--- React/app-react-0/.eslintrc.cjs
+++ React/app-react-0/.eslintrc.cjs
@@ -1,37 +1,36 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-    },
-    extends: [
-      'standard-with-typescript',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      project: './tsconfig.json',
-    },
-    plugins: [
-      'react',
-      '@typescript-eslint',
-    ],
-    rules: {
-      // Aquí puedes agregar tus reglas personalizadas
-      'react/react-in-jsx-scope': 'off',
-    },
-    overrides: [
-      {
-        env: {
-          node: true,
-        },
-        files: ['.eslintrc.{js,cjs}'],
-        parserOptions: {
-          sourceType: 'script',
-        },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  },
+  overrides: [
+    {
+      env: {
+        node: true
       },
-    ],
-  };
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ]
+}
--- React/app-react-0/src/App.tsx
+++ React/app-react-0/src/App.tsx
@@ -1,8 +1,8 @@
 const App = (): JSX.Element => {
   return (
-    <div className="bg-red-200">
-      <h1 className="text-4xl text-center">Hello, World!</h1>
-    </div>
+    <section className='bg-red-200'>
+      <h1 className='text-4xl text-center'>Hello, World!</h1>
+    </section>
   )
 }
 
