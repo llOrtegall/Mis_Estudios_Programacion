82068a66697c3c6658df384064a3beb047300c10
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-02-09 22:14:16 -0500

    add mysql and database

--- React/app-react-0/api/src/index.ts
+++ React/app-react-0/api/src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(cors())
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
