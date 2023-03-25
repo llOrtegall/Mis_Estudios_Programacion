d18a31d0e403cb775669822749f87df5a4f9bbb5
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-25 10:20:19 -0500

    ejemplo de varios botones

--- React/VanilaJS/main.js
+++ React/VanilaJS/main.js
@@ -1,13 +1,14 @@
-const button = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 
-button.addEventListener('click', function () {
-
-  const id = this.getAttribute('data-id');
-  if (button.classList.contains('liked')) {
-    button.classList.remove('liked');
-    button.innerHTML = 'Me Gusta';
-  } else {
-    button.classList.add('liked');
-    button.innerHTML = 'Quitar Me Gusta';
-  }
+buttons.forEach(button => {
+  button.addEventListener('click', function () {
+    const id = this.getAttribute('data-id');
+    if (button.classList.contains('liked')) {
+      button.classList.remove('liked');
+      button.innerHTML = 'Me Gusta';
+    } else {
+      button.classList.add('liked');
+      button.innerHTML = 'Quitar Me Gusta';
+    }
+  })
 })
