d81dd71a31ee7cc1cfaeb6a64bc417e000421744
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-29 08:08:03 -0500

    agregando un componente

---
+++ React/React02/src/App.jsx
@@ -0,0 +1,17 @@
+export function App() {
+  return (
+    <article>
+      <header>
+        <img src="https://unavatar.io/instagram/willsmith" alt="Avatar De Prueba" />
+        <div>
+          <strong>Ivan Ortega Garzon</strong>
+          <span>@llOrTeGall</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  )
+}
--- React/React02/src/main.jsx
+++ React/React02/src/main.jsx
@@ -1,18 +1,11 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import { App } from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const Buttom = ({ text }) => {
-  return (
-    <button>{text}</button>
-  )
-}
-
 root.render(
-  <React.Fragment>
-    <Buttom text='button 1' />
-    <Buttom text='button 2' />
-    <Buttom text='button 3' />
-  </React.Fragment>
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
