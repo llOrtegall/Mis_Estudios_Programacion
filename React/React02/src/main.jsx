4a1ea6364dc9a570df68d4d01cb771b6fdc90e4b
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-29 11:00:14 -0500

    ejemplo componente

--- React/React02/src/main.jsx
+++ React/React02/src/main.jsx
@@ -2,4 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render('Hola mundo')
+
+const Buttom = ({ text }) => {
+  return (
+    <button>{text}</button>
+  )
+}
+
+root.render(
+  <React.Fragment>
+    <Buttom text='button 1' />
+    <Buttom text='button 2' />
+    <Buttom text='button 3' />
+  </React.Fragment>
+)
