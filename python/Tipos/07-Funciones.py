db02679b74083622acddf9e77bca713c7276aff1
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-04-01 19:32:09 -0500

    math

--- python/Tipos/07-Funciones.py
+++ python/Tipos/07-Funciones.py
@@ -1,1 +1,11 @@
+import math
+
 print(round(1.3))
+print(abs(-55))
+
+print(math.ceil(1.1))
+print(math.floor(1.999))
+print(math.isnan(23))
+# print(math.isnan("23"))
+print(math.pow(10, 3))
+print(math.sqrt(9))
