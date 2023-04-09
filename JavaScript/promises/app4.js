43df178ca0ca6031c6a7918d1a92af63bd4ed364
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-04-08 20:15:45 -0500

    first commit

---
+++ JavaScript/promises/app4.js
@@ -0,0 +1,30 @@
+const paises = [];
+
+const nuevoPais = pais => new Promise(resolve => {
+    setTimeout(() => {
+        paises.push(pais);
+        resolve(`Agregado: ${pais}`);
+    }, 3000)
+
+})
+
+nuevoPais('Alemania')
+    .then(resultado => {
+        console.log(resultado);
+        console.log(paises);
+        return nuevoPais('Brasil');
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(paises);
+        return nuevoPais('Argentina');
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(paises);
+        return nuevoPais('Colombia');
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(paises);
+    })
