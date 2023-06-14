cd7d5b781d9c4d2b70200f2354122eeddb799f94
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-14 18:52:58 -0500

    strings

---
+++ python/Tipos/01-Variables.py
@@ -0,0 +1,8 @@
+nombre_curso = "Ultimate Python 1"
+nombre1 = "Ultime Python 2"
+NOMBRE_CURSO = "Ultime Python 3"
+NombreCurso = "Ultime Python 4"
+print(nombre_curso, nombre1, NOMBRE_CURSO)
+alumnos = 5000  # Integer
+puntaje = 9.9  # Float
+publicado = True  # Boolean
