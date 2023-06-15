bb344292f861afa1a257c39ba63f38de60259a8f
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-15 10:59:20 -0500

    anotaciones type Script

---
+++ TS/02-class.ts
@@ -0,0 +1,7 @@
+//* TypeScript no funciona en tiempo de ejecución <---------
+//* JavaScript Funciona en tiempo de ejecución <---------
+
+
+// TODO: anotaciones de types --> agregamos el tipo despues de nombrar la variable
+const nombre: string = 'Miguel';
+
