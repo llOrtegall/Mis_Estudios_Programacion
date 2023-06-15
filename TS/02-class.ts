b0a0bd827419353605ed62ecce53d66e0b6b1fff
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-15 11:03:47 -0500

    inferencia ejem 1

--- TS/02-class.ts
+++ TS/02-class.ts
@@ -3,5 +3,15 @@
 
 
 // TODO: anotaciones de types --> agregamos el tipo despues de nombrar la variable
-const nombre: string = 'Miguel';
+const persona: string = 'Miguel';
 
+const persona2 = {
+    name: 'Pepe',
+    age: 30
+}
+
+//*Type Script Usa inferencia es capaz de mostrarte como es el object en este ejemplo
+persona2.age = 12;
+persona2.name = 'Ivan'
+
+// ? Curiosidad VS Code Esta desarrollado con TypeScript y TypeScript con TypeScript
