f5db918c7b42bea61c34ea3540f80baa166857fd
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-18 15:35:52 -0500

    ejemplo 2

---
+++ TS/07-class.ts
@@ -0,0 +1,47 @@
+// // Union Types
+
+// type HeroId = `${string}-${string}-${string}-${string}-${string}`
+// type HerroPoweScale = 'local' | 'planetary' | 'galactic' | 'universal' | 'multiversal'
+
+// // let a: number | string
+// // a = 1 a = 'Hola'
+
+// type Heerro = {
+//     readonly id?: HeroId
+//     name: string
+//     age: number,
+//     isActive?: boolean
+//     powerScale?: HerroPoweScale
+// }
+
+// let heerro: Heerro = {
+//     name: 'Thor',
+//     age: 36
+// }
+
+// function createHerro4(heerro: Heerro): Heerro {
+//     const { name, age } = heerro
+//     return {
+//         id: crypto.randomUUID(),
+//         name,
+//         age,
+//         isActive: true,
+//     }
+// }
+
+// const thor5 = createHerro4({ name: 'Thor', age: 36 })
+// thor5.powerScale = "multiversal"
+
+// Types Index
+type HeroProperties = {
+    isActive: boolean,
+    adress: {
+        planet: string,
+        city: string
+    }
+}
+
+const addresHero: HeroProperties['adress'] = {
+    planet: 'Earth',
+    city: 'Madrid'
+}
