4dcf0152e6ebb6a35762db3062324fc34d7565e2
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-18 15:46:58 -0500

    tupples

--- TS/08-class.ts
+++ TS/08-class.ts
@@ -1,4 +1,20 @@
 // Arrays
-const lenguages = []
+// * const lenguages = [] // que siempre este vacio
+const lenguages: (string | number)[] = [];
+const lenguages2: Array<string> = []; //TODO: otra forma de declarar el array con el type
 
 lenguages.push('JavaScript')
+lenguages.push('Python')
+
+type cellValue = 'X' | 'O' | '';
+type GameBoard = [
+    [cellValue, cellValue, cellValue],
+    [cellValue, cellValue, cellValue],
+    [cellValue, cellValue, cellValue],
+]
+
+const gameBoard: GameBoard = [
+    ['X', 'O', 'X'],
+    ['X', 'O', 'X'],
+    ['X', 'O', 'X']
+]
