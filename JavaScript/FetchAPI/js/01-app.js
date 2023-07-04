df94ec79eae60fe8828d4ed31bbcfce0d9487b4c
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-04 10:58:16 -0500

    agrganod caths

--- JavaScript/FetchAPI/js/01-app.js
+++ JavaScript/FetchAPI/js/01-app.js
@@ -3,11 +3,22 @@ cargarTxtBtn.addEventListener('click', obtenerDatos);
 
 function obtenerDatos() {
 
-  const url = '../data/datos.txt';
+  const url = '/JavaScript/FetchAPI/data/datos.txt';
 
+  // fetch API contiene el reject y resolve
   fetch(url)
     .then(repuesta => {
-      console.log(repuesta);
-      console.log(repuesta.status);
+      /** console.log(repuesta);
+      console.log('El Status Es: ' + repuesta.status);
+      console.log(repuesta.statusText);
+      console.log(url);
+      console.log(typeof (repuesta));**/
+      return repuesta.text()
+    })
+    .then(datos => {
+      console.log(datos);
+    })
+    .catch(error => {
+      console.log(error);
     })
 }
