aa03c36ac98ad7151762aff0133a78796a891595
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-04 10:49:20 -0500

    guardar cambios arreglar fetch

---
+++ js/FetchAPI/js/01-app.js
@@ -0,0 +1,14 @@
+const cargarTxtBtn = document.querySelector('#cargarTxt');
+cargarTxtBtn.addEventListener('click', obtenerDatos);
+
+function obtenerDatos() {
+
+  const url = '../data/datos.txt';
+
+  fetch(url)
+    .then(repuesta => {
+      console.log(repuesta);
+      console.log(repuesta.status);
+    })
+}
+
