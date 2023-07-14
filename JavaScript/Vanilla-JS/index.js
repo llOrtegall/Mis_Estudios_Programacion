2530bb39960a6b2e14da32bdb644e84f2d624ed2
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-14 09:58:10 -0500

    button liked ok

--- JavaScript/Vanilla-JS/index.js
+++ JavaScript/Vanilla-JS/index.js
@@ -4,6 +4,13 @@ button.addEventListener('click', () => {
   // recuperar el id
   const id = button.getAttribute('data-id');
 
-  //
+  if (button.classList.contains('liked')) {
+    button.classList.remove('liked')
+    button.innerText = 'Me gusta'
+  } else {
+    button.classList.add('liked')
+    button.innerText = 'Quitar Me Gusta'
+  }
+
 })
 
