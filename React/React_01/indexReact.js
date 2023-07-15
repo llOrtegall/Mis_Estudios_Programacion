58ad8f94e036c1f53a5da28d422cf02cd3f571df
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-15 10:23:31 -0500

    fragment react

--- React/React_01/indexReact.js
+++ React/React_01/indexReact.js
@@ -4,6 +4,16 @@ import React from "https://esm.sh/react@18.2.0";
 const appDomElement = document.getElementById('app');
 
 const root = ReactDOM.createRoot(appDomElement);
-const button = React.createElement('button', null, 'Me Gusta')
 
-root.render()
+
+
+const button = React.createElement('button', { "data-id": 123 }, 'Me Gusta 1')
+const button1 = React.createElement('button', { "data-id": 456 }, 'Me Gusta 2')
+const button2 = React.createElement('button', { "data-id": 789 }, 'Me Gusta 3')
+
+const div = React.createElement(React.Fragment, null, [button, button1, button2])
+
+root.render(div /* button1, button2 */)
+
+/* const suma = (a, b) => 2 + 2
+suma(2, 3, 5) */
