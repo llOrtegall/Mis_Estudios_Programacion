8f94813894b99f4ed78a14fa605ec4a3010d22b5
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-15 10:17:42 -0500

    react impro y create element

--- React/React_01/indexReact.js
+++ React/React_01/indexReact.js
@@ -1,7 +1,9 @@
 import ReactDOM from "https://esm.sh/react-dom@18.2.0/client";
+import React from "https://esm.sh/react@18.2.0";
 
 const appDomElement = document.getElementById('app');
 
 const root = ReactDOM.createRoot(appDomElement);
+const button = React.createElement('button', null, 'Me Gusta')
 
-root.render('Hola')
+root.render()
