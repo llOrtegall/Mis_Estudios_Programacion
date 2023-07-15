2fac304ee3a0202f089bde04ed73a04a33571511
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-15 10:11:39 -0500

    hacer funcionar React

---
+++ React/React_01/indexReact.js
@@ -0,0 +1,7 @@
+import ReactDOM from "https://esm.sh/react-dom@18.2.0/client";
+
+const appDomElement = document.getElementById('app');
+
+const root = ReactDOM.createRoot(appDomElement);
+
+root.render('Hola')
