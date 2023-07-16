d81893630d8928e5755d6be4ce61302e21b3ef05
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-16 10:53:24 -0500

    hello bootun

--- React/React_02/src/main.jsx
+++ React/React_02/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.Fragment>
+    <button>Hola button</button>
+    <button>Hola button</button>
+    <button>Hola button</button>
+  </React.Fragment>
 )
