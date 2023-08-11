f132193ee7d1c93c7b69469e0ab2e0b2cf8ea1b9
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-11 07:14:44 -0500

    primero pasos

--- React/React03/src/App.jsx
+++ React/React03/src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+
+// TODO: Esto es un componente y Devuelven Elementos
+const Home = () => <h1>Home</h1>
+const SearchPage = () => <h1>Search</h1>
+
+
+
+export function App() {
   return (
     <section>
-      <h1>Hello</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<SearchPage />} />
+      </Routes>
     </section>
   )
 }
-
-export default App
--- React/React03/src/main.jsx
+++ React/React03/src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { App } from './App.jsx'
 import './index.css'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
