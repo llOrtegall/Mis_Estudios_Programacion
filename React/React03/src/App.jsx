9c6296a1feb99f09d4a8915378180ba77a83b581
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-14 07:24:00 -0500

    como redireccionar

--- React/React03/src/App.jsx
+++ React/React03/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 
 // TODO: Esto es un componente y Devuelven Elementos
 const Home = () => <h1>Home</h1>
@@ -6,13 +6,13 @@ const SearchPage = () => <h1>Search</h1>
 
 export function App() {
   return (
-    <section>
+    <>
       <header className="flex justify-between p-4 bg-blue-300">
         <h1 className="font-bold text-purple-600">App De Prueba</h1>
         <nav>
           <ul className="flex px-4">
-            <li className="mx-6"><a href="/search">Search</a></li>
-            <li className="mx-6"><a href="/">Home</a></li>
+            <li className="mx-6"><Link to="/search">Search</Link></li>
+            <li className="mx-6"><Link to="/">Home</Link></li>
           </ul>
         </nav>
       </header>
@@ -20,6 +20,6 @@ export function App() {
         <Route path='/' element={<Home />} />
         <Route path='/search' element={<SearchPage />} />
       </Routes>
-    </section>
+    </>
   )
 }
