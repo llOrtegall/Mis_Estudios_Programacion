9c0f486fc0ef8eaa342b6c138dcff0b7d10d1866
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-16 07:37:46 -0500

    rutas dinamicas

--- React/React03/src/App.jsx
+++ React/React03/src/App.jsx
@@ -2,23 +2,45 @@ import { Route, Routes, Link } from "react-router-dom";
 
 // TODO: Esto es un componente y Devuelven Elementos
 const Home = () => <h1>Home</h1>
-const SearchPage = () => <h1>Search</h1>
+
+const Tacos = () => <h1>Tacos</h1>
+
+const SearchPage = () => {
+  const tacos = ['Cochinita', 'Chile', 'Quesadilla', 'Carnita']
+  return (
+    <div className="w-screen h-screen text-center">
+      <h1>Searh Page</h1>
+      <ul>
+        {tacos.map(taco => (
+          <li key={taco}>
+            <Link className="mx-2" to={`/tacos/${taco}`} >{taco}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export function App() {
   return (
     <>
       <header className="flex justify-between p-4 bg-blue-300">
-        <h1 className="font-bold text-purple-600">App De Prueba</h1>
+        <h1 className="font-bold text-purple-600">Company Name</h1>
         <nav>
           <ul className="flex px-4">
-            <li className="mx-6"><Link to="/search">Search</Link></li>
-            <li className="mx-6"><Link to="/">Home</Link></li>
+            <li className="mx-6 font-semibold hover:text-yellow-200">
+              <Link to="/search">Search</Link>
+            </li>
+            <li className="mx-6 font-semibold hover:text-yellow-200">
+              <Link to="/">Home</Link>
+            </li>
           </ul>
         </nav>
       </header>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/search' element={<SearchPage />} />
+        <Route path="tacos/:nombre-del-taco" element={<Tacos />} />
       </Routes>
     </>
   )
