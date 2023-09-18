77f3593467271ae74436580472d8596ab382e4fb
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-18 10:14:35 -0500

    continuar con clase de arreglos

---
+++ JavaScript/Arreglos/Arreglo03.js
@@ -0,0 +1,10 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio']
+
+console.table(meses)
+
+// TODO: Recorrer un Arreglo
+console.log(meses.length);
+
+for (let i = 0; i < meses.length; i++) {
+  console.log(meses[i]);
+}
