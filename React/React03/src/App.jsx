fe779c9104846d3250aa8dfb04b238291ed3252a
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-19 08:45:54 -0500

    ejemplo con array useRoutes

--- React/React03/src/App.jsx
+++ React/React03/src/App.jsx
@@ -1,66 +1,36 @@
-import { Route, Routes, Link, useParams, Outlet } from "react-router-dom";
-import { NavLink } from "./NavLink.jsx";
-// TODO: Esto es un componente y Devuelven Elementos
-const Home = () => <h1>Home</h1>
+import { useRoutes, Link } from "react-router-dom"
 
-const SearchPage = () => {
-  const tacos = ['Cochinita', 'Chile', 'Quesadilla', 'Carnita']
-  return (
-    <div className="w-auto h-auto text-center">
-      <h1>Searh Page</h1>
-      <ul>
-        {tacos.map(taco => (
-          <li key={taco}>
-            <Link className="mx-2" to={`/tacos/${taco}`} >{taco}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+const routes = [
+  {
+    path: '/',
+    element: (
+      <div>
+        <h2>HOME</h2>
+        <Link to='/search' >Go to home</Link>
+      </div>
+    )
+  },
+  {
+    path: '/search',
+    element: (
+      <div>
+        <h2>SEARCH</h2>
+        <Link to='/' >Go to home</Link>
+      </div>
+    )
+  }
+]
 
-const Tacos = () => {
-  const { tacoName } = useParams()
-  return (
-    <div>
-      <h1>{tacoName}</h1>
-      <Link to='details' >Ir A los Detalles</Link>
-      <Outlet />
-    </div>
-  )
-}
-
-
-const TacoDetails = () => {
-  const { tacoName } = useParams()
-  return (
-    <h1>Taco Details del {tacoName}</h1>
-  )
-}
 export function App() {
+
+  const element = useRoutes(routes)
+
   return (
-    <>
-      <header className="flex justify-between p-4 bg-blue-300">
-        <h1 className="font-bold text-purple-600">Company Name</h1>
-        <nav>
-          <ul className="flex px-4">
-            <li className="mx-6 hover:text-yellow-200">
-              <NavLink to="/search">Search</NavLink>
-            </li>
-            <li className="mx-6 hover:text-yellow-200">
-              <NavLink to="/">Home</NavLink>
-            </li>
-          </ul>
-        </nav>
+    <main>
+      <header>
+        <h1>Ortega</h1>
       </header>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/search' element={<SearchPage />} />
-        <Route path="tacos/:tacoName" element={<Tacos />} >
-          <Route path='details' element={<TacoDetails />} />
-        </Route>
-        <Route path='*' element={<h1>Not Found</h1>} />
-      </Routes>
-    </>
+      {element}
+    </main>
   )
 }
