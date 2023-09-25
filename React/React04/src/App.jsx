357fcaf7580f5b9ca0a360ed563d3044940c804e
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-25 08:50:08 -0500

    primeros pasos

--- React/React04/src/App.jsx
+++ React/React04/src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export function App() {
 
   return (
-    <h1>Home</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<div>About</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
