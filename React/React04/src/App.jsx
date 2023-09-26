186b9a9040403657b6d45aa76a97f39674ba2873
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-26 08:57:57 -0500

    not found pages

--- React/React04/src/App.jsx
+++ React/React04/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AboutPage } from "./pages/AboutPage.jsx";
 import { HomePage } from "./pages/HomePage.jsx";
 import { UsersPage } from "./pages/UsersPage.jsx";
+import { NotFoundPage } from "./pages/NotFoundPage.jsx";
 
 export function App() {
 
@@ -11,6 +12,7 @@ export function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/users" element={<UsersPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   )
