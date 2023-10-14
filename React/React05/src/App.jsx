3a90b629339a291066d95042e00875467ff6cb61
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-14 08:26:53 -0500

    set fact

--- React/React05/src/App.jsx
+++ React/React05/src/App.jsx
@@ -1,5 +1,17 @@
-export function App() {
+import { useEffect, useState } from 'react'
+
+export function App () {
+  const [fact, setFact] = useState('lorem impsun cat fact watjever')
+
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+      .then(res => console.log(res))
+      .then(data => setFact(data.fact))
+  }, [])
+
   return (
-    <h3>App De gatitos</h3>
+    <main>
+      <h1>{fact}</h1>
+    </main>
   )
 }
