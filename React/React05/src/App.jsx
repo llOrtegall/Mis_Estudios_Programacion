393d51ac89924b6984ec40aec60908e916d0df94
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-16 09:59:38 -0500

    esto esta bien pero puede ser mejor

--- React/React05/src/App.jsx
+++ React/React05/src/App.jsx
@@ -7,27 +7,42 @@ export function App () {
   const [fact, setFact] = useState()
   const [catImage, setCatImage] = useState()
 
-  useEffect(() => {
+  const getRamdonFact = () => {
     fetch(CAT_RAMDON_FACT)
       .then(res => res.json())
       .then(data => {
         const { fact } = data
         setFact(fact)
-
-        const threeFirstWords = fact.split(' ', 3).join()
-
-        fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=20&fontColor=red`)
-          .then(response => response)
-          .then(response => {
-            const { url } = response
-            setCatImage(url)
-          })
       })
-  }, [])
+  }
+
+  // TODO: Aquí Tenemos Un Effect
+  useEffect(getRamdonFact, [])
+
+  // TODO: Para Recuperar la imagen
+  useEffect(() => {
+    if (!fact) return
+
+    const threeFirstWords = fact.split(' ', 3).join()
+
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=20&fontColor=red`)
+      .then(response => response)
+      .then(response => {
+        const { url } = response
+        setCatImage(url)
+      })
+  }, [fact])
+
+  const handleClick = () => {
+    getRamdonFact()
+  }
 
   return (
     <main>
       <h2>App De Gatitos</h2>
+      <button onClick={handleClick}>
+        Refrescar Gatito
+      </button>
       <section>
         {fact && <p>{fact}</p>}
         {catImage && <img src={catImage} alt={`image cat says words ${fact}`} />}
