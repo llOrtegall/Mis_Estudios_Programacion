1349ca6a40394573cef990de21ddc464e28d2f7b
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-20 08:32:14 -0500

    proyecto tremo para mostrar propuesta

--- React/app-react-1/src/App.tsx
+++ React/app-react-1/src/App.tsx
@@ -1,9 +1,26 @@
-const App: React.FC = () => {
+import { DndContext, closestCenter } from '@dnd-kit/core';
+import { useState } from 'react';
+
+function App() {
+  const [people, setPeople] = useState([
+    { id: '1', name: 'John' },
+    { id: '2', name: 'Paul' },
+    { id: '3', name: 'George' },
+    { id: '4', name: 'Ringo' },
+    { id: '5', name: 'Brian' },
+    { id: '6', name: 'George Martin' },
+  ]);
+
+  const handleDragEnd = (e) => { }
+
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <DndContext collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}>
+      <h1 className='text-center'>User List</h1>
+    
+
+    </DndContext>
   )
 }
 
-export default App
+export default App;
