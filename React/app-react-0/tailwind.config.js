a0b3afc78aa6b7451a24e4a1c5f3f2af343485a7
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-21 22:55:15 -0500

    tailwind agregado ok

---
+++ React/app-react-0/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    dark: 'class',
+    extend: {
+      colors: {
+        'primary': '#3490dc',
+        'secondary': '#ffed4a',
+        'danger': '#e3342f',
+      },
+      fontFamily: {
+        'sans': ['Graphik', 'sans-serif'],
+        'serif': ['Merriweather', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
