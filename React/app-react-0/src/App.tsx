8eed2c580b7ba6d7c8ea9f41c12286bf940d390b
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-21 22:34:42 -0500

    lazy implementado

--- React/app-react-0/src/App.tsx
+++ React/app-react-0/src/App.tsx
@@ -1,21 +1,25 @@
+import { Suspense, lazy } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import Dashboard from './pages/Dashboard'
 import NavBar from './components/NavBar'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
+
+const Dashboard = lazy(() => import('./pages/Dashboard'))
+const Contact = lazy(() => import('./pages/Contact'))
+const About = lazy(() => import('./pages/About'))
+const Home = lazy(() => import('./pages/Home'))
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
