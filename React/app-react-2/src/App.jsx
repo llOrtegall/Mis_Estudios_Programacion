51bc364b0fc2a661799b3b557154c20594693331
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-04-02 16:37:11 -0500

    validar este funcionamiento

--- React/app-react-2/src/App.jsx
+++ React/app-react-2/src/App.jsx
@@ -1,37 +1,87 @@
-import React, {useState} from 'react';
-import {DndContext} from '@dnd-kit/core';
-
-import {Droppable} from './Droppable';
-import {Draggable} from './Draggable';
+import { SortableContext, useSortable, verticalListSortingStrategy } from '@dnd-kit/sortable'
+import { DndContext, closestCenter } from '@dnd-kit/core'
+import { CSS } from '@dnd-kit/utilities'
+import { useState } from 'react'
 
 function App() {
-  const containers = ['A', 'B'];
-  const [parent, setParent] = useState(null);
-  const draggableMarkup = (
-    <Draggable id="draggable">Drag me</Draggable>
-  );
+  const [items, setItems] = useState([
+    { id: 1, name: 'Item 1', list: 'items' },
+    { id: 2, name: 'Item 2', list: 'items' },
+    { id: 3, name: 'Item 3', list: 'items' },
+    { id: 4, name: 'Item 4', list: 'items2' },
+    { id: 5, name: 'Item 5', list: 'items2' },
+    { id: 6, name: 'Item 6', list: 'items2' }
+  ])
+
+  const handleDragEnd = (ev) => {
+    const { active, over } = ev
+
+    if (active && over) {
+      setItems(items => {
+        const activeItem = items.find(item => item.id === active.id)
+        const overItem = items.find(item => item.id === over.id)
+
+        if (activeItem && overItem) {
+          return items.map(item => {
+            if (item.id === active.id) {
+              return { ...item, list: overItem.list }
+            } else {
+              return item
+            }
+          })
+        } else {
+          return items
+        }
+      })
+    }
+  }
+
+  const items1 = items.filter(item => item.list === 'items')
+  const items2 = items.filter(item => item.list === 'items2')
 
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      {parent === null ? draggableMarkup : null}
+    <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
 
-      {containers.map((id) => (
-        // We updated the Droppable component so it would accept an `id`
-        // prop and pass it to `useDroppable`
-        <Droppable key={id} id={id}>
-          {parent === id ? draggableMarkup : 'Drop here'}
-        </Droppable>
-      ))}
+      <section className='flex w-full gap-2 px-2'>
+
+        <div className='w-full flex flex-col'>
+          <SortableContext items={items1} strategy={verticalListSortingStrategy}>
+            {items1.map(item => (<Item key={item.id} item={item} />))}
+          </SortableContext>
+        </div>
+
+        <div className='w-full flex flex-col'>
+          <SortableContext items={items2} strategy={verticalListSortingStrategy}>
+            {items2.map(item => (<Item key={item.id} item={item} />))}
+          </SortableContext>
+        </div>
+
+      </section>
+
     </DndContext>
-  );
+  )
+}
 
-  function handleDragEnd(event) {
-    const {over} = event;
+export default App
 
-    // If the item is dropped over a container, set it as the parent
-    // otherwise reset the parent to `null`
-    setParent(over ? over.id : null);
+function Item({ item }) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: item.id })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
   }
-};
 
-export default App;
+  return (
+    <div {...attributes} {...listeners} ref={setNodeRef} style={style}
+      className='bg-slate-300 my-2 rounded-md p-4'>
+      {item.name}
+    </div>
+  )
+}
