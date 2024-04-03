256d1549c79962c31f74f324a8a1b4d509dd6b67
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-04-02 20:38:32 -0500

    dependencias instaladas tailwind agregado y primer compont

---
+++ React/app-react-2/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <section>
+      <h1>React App</h1>
+    </section>
+  )
+}
+
+export default App
---
+++ React/app-react-2/src/components/KanbanBoard.tsx
@@ -0,0 +1,9 @@
+function KanbanBoard() {
+  return (
+    <div>
+      <h1>Kanban Board</h1>
+    </div>
+  )
+}
+
+export default KanbanBoard
