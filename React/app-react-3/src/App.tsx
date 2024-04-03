0a6c26776060ed66a2fefe790e15c964e91454bf
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-04-03 16:09:50 -0500

    antes de aplica handle DragOver

--- React/app-react-3/src/App.tsx
+++ React/app-react-3/src/App.tsx
@@ -121,16 +121,23 @@ function RenderBodega({ bodg }: { bodg: Bodega }) {
 
 function RenderItem({ item }: { item: Item }) {
 
-  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: item.id, data: { type: 'item', item }})
-
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id: item.id, data: { type: 'item', item }})
+ 
   const style = {
     transition,
     transform: CSS.Transform.toString(transform),
   }
 
+  if (isDragging) {
+    return (
+      <div ref={setNodeRef} style={style}
+        className="flex w-[300px] h-[42px] justify-around my-4 py-2 border rounded-lg bg-sky-200 text-black cursor-grab opacity-30" />
+    )
+  }
+
   return (
     <section ref={setNodeRef} {...attributes} {...listeners} style={style}
-      className="flex justify-around my-4 py-2 border rounded-lg bg-sky-200 text-black">
+      className="flex w-[300px] h-[42px] justify-around my-4 py-2 border rounded-lg bg-sky-200 text-black">
       <p><span className="font-bold">Nombre: </span>{item.nombre}</p>
       <p><span className="font-bold">Precio: </span>{item.precio}</p>
     </section>
