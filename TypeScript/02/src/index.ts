0a4aa3b145b7954b9960908ac75c62d07272a04b
llOrtegall <IvanOrtega_97@hotmail.com>
2024-07-04 09:14:03 -0500

    implementando interfaces

--- TypeScript/02/src/index.ts
+++ TypeScript/02/src/index.ts
@@ -14,7 +14,7 @@ class Sale {
 class SaleWithTas extends Sale {
   private tax: number;
 
-  constructor (tax: number, amount: number) {
+  constructor(tax: number, amount: number) {
     super(amount); // call the parent constructor
     this.tax = tax;
   }
@@ -29,14 +29,87 @@ const res = sale.getTotal();
 let saleWithTax = new SaleWithTas(50, 300);
 const res2 = saleWithTax.getTotal();
 
+// TODO: Types
 type Beer = {
   name: string,
   price: number,
   alcohol: number
 }
 
-function show (beer: Beer) {
+function show(beer: Beer) {
   console.log(beer);
 }
 
-show({name: 'Corona', price: 50, alcohol: 4.5});
+show({ name: 'Corona', price: 50, alcohol: 4.5 });
+
+
+// TODO: Interfaces
+interface Drink {
+  name: string,
+  price: number,
+}
+
+interface AlcoholLicDrink extends Drink {
+  alcohol: number
+  showInfo(): string
+}
+
+interface MixedDrink {
+  ingredients: string[]
+}
+
+const drink1: Drink = {
+  name: 'Redbull',
+  price: 120
+}
+
+class Wine implements AlcoholLicDrink {
+  alcohol: number;
+  price: number;
+  name: string;
+
+  constructor(name: string, alcohol: number, price: number) {
+    this.name = name;
+    this.alcohol = alcohol;
+    this.price = price;
+  }
+
+  showInfo(): string {
+    return `Drink: ${this.name}, Alcohol: ${this.alcohol}`;
+  }
+}
+
+class CokTail implements AlcoholLicDrink, MixedDrink {
+  alcohol: number;
+  name: string;
+  ingredients: string[];
+  price: number;
+
+  constructor(name: string, alcohol: number, price: number, ingredients: string[]) {
+    this.name = name;
+    this.alcohol = alcohol;
+    this.ingredients = ingredients;
+    this.price = price;
+  }
+
+  showInfo(): string {
+
+    const ingredientsInfo = this.ingredients.reduce((acc, element) => acc + " " + element + ",", "");
+
+    return `Drink: ${this.name}, Alcohol: ${this.alcohol}, Ingredients: ${ingredientsInfo}`
+  }
+}
+
+const margarita = new CokTail('Margarita', 10, 150, ['Lemon', 'Salt', 'Tequila']);
+const rioja = new Wine('Rioja', 12, 200);
+const malbet = new Wine('Malbet', 14, 250);
+
+const ad: AlcoholLicDrink[] = [
+  margarita, rioja, malbet
+]
+
+function showDrinks (drinks: AlcoholLicDrink[]) {
+  drinks.forEach(e => console.log(e.showInfo()));
+}
+
+showDrinks(ad);
