2bb832c1a099a812498e5f03951100173994bb17
llOrtegall <IvanOrtega_97@hotmail.com>
2024-07-05 14:54:51 -0500

    tailwind agregado revisar logica flujo app

--- React/app-react-0/client/src/pages/LoginPage.tsx
+++ React/app-react-0/client/src/pages/LoginPage.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom'
+
 function LoginPage () {
   return (
     <div>
       <h1>Login</h1>
+      <Link to={'/home'}>Ir a Home</Link>
     </div>
   )
 }
--- React/app-react-0/client/src/pages/NotFound.tsx
+++ React/app-react-0/client/src/pages/NotFound.tsx
@@ -1,24 +1,23 @@
-import { useRouteError } from 'react-router-dom'
+import { Link, useRouteError } from 'react-router-dom'
 
 function NotFound () {
   const error = useRouteError()
 
-  // Log the error for debugging purposes
-  console.error(error)
-
-  // Type assertion: Assuming error is an object with optional properties `statusText` and `message`
   const errorMessage = typeof error === 'object' && error !== null
     ? (error as { statusText?: string; message?: string }).statusText || (error as { message?: string }).message
     : 'An unknown error occurred'
 
   return (
-    <div id="error-page">
+    <section>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{errorMessage}</i>
       </p>
-    </div>
+      <div>
+        <Link to="/">Go back to the home page</Link>
+      </div>
+    </section>
   )
 }
 
