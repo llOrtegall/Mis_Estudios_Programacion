26cbd2dd2ecbf011eabfe6ba634c46dd3a226c17
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-07-18 16:19:29 -0500

    crear implmentación para crear tables

--- Node/sequalize/src/index.ts
+++ Node/sequalize/src/index.ts
@@ -2,11 +2,24 @@ import { Sucursales } from './models/sucursales.model'
 
 async function getAllSucursales() {
   try {
-    const sucursales = await Sucursales.findAll()
-    console.log(sucursales)
+    const sucursales = await Sucursales.findAll({
+      attributes: ['CODIGO', 'NOMBRE'],
+      where: { ZONA: 39627 }
+    })
+    
+    const sucursalesArray = sucursales.map(sucursal => {
+      return sucursal.dataValues.CODIGO
+    })
+
+    console.log(sucursalesArray.length);
+
+    return sucursalesArray
   } catch (error) {
     console.error(error)
   }
 }
 
 getAllSucursales()
+  .then(res => {
+    console.log(res)
+  })
