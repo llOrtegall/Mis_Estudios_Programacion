21c82e2f3f01317de3f6bd52e6431a51895e8bf8
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-08-15 23:02:43 -0500

    firts example easy to components react

--- React/app-00-react/src/main.tsx
+++ React/app-00-react/src/main.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import { StrictMode } from 'react'
-import App from './App.tsx'
-import './index.css'
+import { Button } from './components/Button'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
+  <>
+    <Button text="Button 1" />
+    <Button text="Button 2" />
+    <Button text="Button 3" />
+  </>
 )
