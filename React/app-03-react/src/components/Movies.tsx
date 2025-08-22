6337ae6a9d7dcd08bd274dfd96335145c88aae18
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-08-21 21:25:56 -0500

    created component movies to render list movies

---
+++ React/app-03-react/src/components/Movies.tsx
@@ -0,0 +1,15 @@
+import type { Movie } from "../types/movies";
+
+export function Movies({ movies }: { movies: Movie[] }) {
+  return (
+    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <h3>{movie.title}</h3>
+          <p>{movie.year}</p>
+          <img src={movie.image} alt={movie.title} />
+        </li>
+      ))}
+    </ul>
+  )
+}
