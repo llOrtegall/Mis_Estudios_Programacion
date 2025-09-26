e5cc5db9e7fc94838f61d7746710c829351447ff
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-09-25 21:26:28 -0500

    feat: add examples of Object.groupBy with wizards and avengers arrays

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -40,3 +40,24 @@ const grouped2 = Object.groupBy(
 )
 
 console.log(grouped2)
+
+const wizzards = ['Harry', 'Hermione', 'Ron', 'Severus', 'Hagrid']
+
+const groupedWizzards = Object.groupBy(wizzards, wizzard => wizzard[0])
+
+console.log(groupedWizzards)
+
+
+const avangers = [
+    { name: 'Spiderman', age: 16 },
+    { name: 'Black Widow', age: 34 },
+    { name: 'Hulk', age: 49 },
+    { name: 'Thor', age: 1500 },
+    { name: 'Iron Man', age: 40 },
+    { name: 'Hawkeye', age: 34 },
+    { name: 'Black Panther', age: 49 },
+]
+
+const groupedAvangers = Object.groupBy(avangers, avanger => avanger.age)
+
+console.log(groupedAvangers)
